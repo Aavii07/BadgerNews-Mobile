b1157f7232febf6cb5b80f8d87f27aa2c6ed15c7
Avi Shukla <avi.l.shukla@gmail.com>
2024-06-22 13:32:14 -0700

    added main news screen

--- src/components/navigation/BadgerTabs.jsx
+++ src/components/navigation/BadgerTabs.jsx
@@ -1,9 +1,38 @@
-import { Text } from "react-native";
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import BadgerNewsScreen from '../screens/BadgerNewsScreen';
+import BadgerPreferencesScreen from '../screens//BadgerPreferencesScreen';
+import Icon from 'react-native-vector-icons/FontAwesome'; // this library doesn't cause weird errors
+
+const Tab = createBottomTabNavigator();
 
 function BadgerTabs(props) {
-    return <>
-        <Text style={{paddingTop: 128}}>Hello World! I should make this tabs instead.</Text>
-    </>
+
+    const screenOptions = ({ route }) => ({
+
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+      
+          if (route.name === 'News') {
+            iconName = "newspaper-o"
+          } else if (route.name === 'Preferences') {
+            iconName = "cogs"
+          }
+      
+          return (
+            <Icon name={iconName} size={size} color={color}/>
+          );
+        },
+      });
+
+    return (
+        <Tab.Navigator screenOptions={screenOptions}>
+
+            <Tab.Screen name="News" component={BadgerNewsScreen} />
+            <Tab.Screen name="Preferences" component={BadgerPreferencesScreen} />
+
+        </Tab.Navigator>
+    );
 }
 
 export default BadgerTabs;
--- src/components/screens/BadgerNewsScreen.jsx
+++ src/components/screens/BadgerNewsScreen.jsx
@@ -1,9 +1,50 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, ActivityIndicator } from "react-native";
+// can use activity indicator instead of loading text (gives a spinning circle)
+import { useState, useEffect } from "react";
+import { Card, Title } from 'react-native-paper';
+import CS571 from '@cs571/mobile-client'
 
 function BadgerNewsScreen(props) {
-    return <View>
+
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true); // used to toggle the activity indicator
+
+    url = "https://cs571api.cs.wisc.edu/rest/su24/hw8/articles"
+
+    useEffect(() =>{
+        fetch(url, {
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId() 
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            setArticles(data);
+            setLoading(false);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    },[]);
+
+    return (
+        <ScrollView>
+            {loading ? (
+                <ActivityIndicator size="large" color="blue" style={{ marginTop: 20 }} />
+            ) : (
+                articles.map((article) => (
+                    <Card key={article.fullArticleId} style={{ margin: 10 }}>
+                        <Card.Cover source={{ uri: "https://raw.githubusercontent.com/CS571-SU24/hw8-api-static-content/main/" + article.img }}/>
+                        <Card.Content style={{ padding: 10 }}>
+                            <Title>{article.title}</Title>
+                        </Card.Content>
+                    </Card>
+                ))
+            )}
+        </ScrollView>
+    );
+
+    /**return <View>
         <Text style={{paddingTop: 128}}>When I get to Step 3, I will need to be inside a nested stack navigator!</Text>
-    </View>
+    </View>  */
 }
 
 export default BadgerNewsScreen;
